09ec92fd66a2149881e443b5da3ce043b7adb781
Alex <sp.savinov@gmail.com>
2023-07-10 18:35:07 +0300

    Добавил список задач и вывод их на экран

---
+++ exercises/Task.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercises
+{
+    internal class Task
+    {
+        public Task(int id, string name, string description, string fullDescription)
+        {
+            Id = id;
+           
+            Name = name;
+            Description = description;
+            FullDescription = fullDescription;
+        }
+
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? FullDescription { get; set; }
+
+
+
+        public void PrintDescription()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Задача #{Id} : {Description}");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("--------------------------------------------------");
+        }
+
+
+        public void PrintFullDescription()
+        {
+            Console.WriteLine("******************************");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Задача #{Id} : {FullDescription}");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("******************************");
+        }
+
+    }
+
+    public class TaskDatabase
+    {
+        
+
+
+    }
+}
---
+++ exercises/Universal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercises
+{
+    internal class Universal
+    {
+        //Получаем данные из строки и конвертируем в тип данных int 
+        static public int ReadInt(string value)
+        {
+            Console.Write(value);
+            return (Convert.ToInt32(Console.ReadLine()));
+        }
+        //Получаем данные из строки и конвертируем в тип данных double 
+        static public double ReadDouble(string value)
+        {
+            Console.Write(value);
+            return (Convert.ToDouble(Console.ReadLine()));
+        }
+        //Выводит список уроков.
+
+
+
+    }
+}
